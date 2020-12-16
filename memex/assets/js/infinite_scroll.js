027a13f89b698408c5b0eebf7d3e7e97c34862c2
Adrian Philipp <adrian@ticketswap.com>
2020-12-16 17:02:41 +0100

    Replace infinite scroll with Interaction Observer
    It's a lot more performance and avoids issues with loading
    a lot of pages at once when scrolling down.

--- memex/assets/js/infinite_scroll.js
+++ memex/assets/js/infinite_scroll.js
@@ -1,21 +1,31 @@
-let scrollAt = () => {
-  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
-  let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
-  let clientHeight = document.documentElement.clientHeight
-
-  return scrollTop / (scrollHeight - clientHeight) * 100
-}
-
 export const InfiniteScroll = {
-  page() { return this.el.dataset.page },
-  mounted(){
-    this.pending = this.page()
-    window.addEventListener("scroll", e => {
-      if(this.pending == this.page() && scrollAt() > 90){
-        this.pending = this.page() + 1
-        this.pushEvent("load-more", {})
-      }
-    })
+  page() {
+    return this.el.dataset.page;
   },
-  updated(){ this.pending = this.page() }
-}
+  loadMore(entries) {
+    const target = entries[0];
+
+    if (target.isIntersecting && this.pending == this.page()) {
+      this.pending = this.page() + 1;
+      this.pushEvent("load-more", {});
+    }
+  },
+  mounted() {
+    this.pending = this.page();
+    this.observer = new IntersectionObserver(
+      (entries) => this.loadMore(entries),
+      {
+        root: null, // window by default
+        rootMargin: "0px",
+        threshold: 1.0,
+      }
+    );
+    this.observer.observe(this.el);
+  },
+  beforeDestroy() {
+    this.observer.unobserve(this.el);
+  },
+  updated() {
+    this.pending = this.page();
+  },
+};
