c104091db81a114fce7af1013a56ffde11cea040
Adrian Philipp <adrian@ticketswap.com>
2020-12-20 16:35:57 +0100

    Add simple query suggestions

--- memex/lib/memex_web/live/page_live.ex
+++ memex/lib/memex_web/live/page_live.ex
@@ -3,9 +3,12 @@ defmodule MemexWeb.PageLive do
 
   alias Memex.Search.Query
 
+  @default_assigns [results: %{}, dates: %{}, metadata: nil, suggestion: nil]
+  @highlight_regex ~r/<em>.*<\/em>(\w*)\W/u
+
   @impl true
   def mount(_params, _session, socket) do
-    socket = assign(socket, query: "", page: 1)
+    socket = assign(socket, query: "", page: 1, suggestion: nil)
     {:ok, socket, temporary_assigns: [results: %{}, dates: %{}, metadata: nil]}
   end
 
@@ -28,6 +31,22 @@ defmodule MemexWeb.PageLive do
     {:noreply, socket |> assign(query: Query.to_string(query), page: 1) |> search()}
   end
 
+  @impl true
+  def handle_event(
+        "accept-suggestion",
+        %{"key" => "ArrowRight"},
+        %{assigns: %{query: query, suggestion: suggestion}} = socket
+      ) do
+    {:noreply,
+     socket
+     |> assign(query: query <> suggestion, suggestion: nil, page: 1)
+     |> push_event("force-input-value", %{value: query <> suggestion})}
+  end
+
+  def handle_event("accept-suggestion", _key, socket) do
+    {:noreply, socket}
+  end
+
   defp search(%{assigns: %{page: page, query: string}} = socket) do
     query = Query.from_string(string)
 
@@ -41,12 +60,26 @@ defmodule MemexWeb.PageLive do
             "totalHits" => response["nbHits"],
             "processingTimeMs" => response["processingTimeMs"]
           },
-          dates: response["facetsDistribution"]["date_month"]
+          dates: response["facetsDistribution"]["date_month"],
+          suggestion: get_suggestion(response["hits"])
         )
 
       _ ->
         socket
-        |> assign(query: string, page: 1, results: %{}, dates: %{}, metadata: nil)
+        |> assign(@default_assigns ++ [query: string, page: 1])
+    end
+  end
+
+  defp get_suggestion(results) do
+    with result when not is_nil(result) <- Enum.at(results, 0) do
+      Enum.find_value(result["_formatted"], nil, fn {_key, value} ->
+        with value when is_binary(value) <- value,
+             [_, found] <- Regex.run(@highlight_regex, value) do
+          found
+        else
+          _ -> false
+        end
+      end)
     end
   end
 end
