6675a876cea12fcbc2e6ed0be86b1bc0f3e545a3
Adrian Philipp <adrian@ticketswap.com>
2021-03-24 16:47:08 +0100

    Rearrange to have faster importers first

--- bin-dev/import-all.sh
+++ bin-dev/import-all.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # Imports all data
-
-./github-to-sqlite/import-database.sh
-./twitter-to-sqlite/import-database.sh
-
-import.sh sqlite-to-json/github.sh
+import.sh sqlite-to-json/git-notes.sh
 import.sh sqlite-to-json/iMessage.sh
 import.sh sqlite-to-json/MoneyMoney.sh
 import.sh sqlite-to-json/Photos.sh
 import.sh sqlite-to-json/Safari.sh
 import.sh sqlite-to-json/terminal.sh
+
+./github-to-sqlite/import-database.sh
+import.sh sqlite-to-json/github.sh
+
+./twitter-to-sqlite/import-database.sh
 import.sh sqlite-to-json/twitter.sh
