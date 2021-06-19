44a446b4af562ae6840f40e951258a25a4b287ff
Adrian Philipp <adrian@ticketswap.com>
2021-06-19 13:20:35 +0200

    Arc > Import activities

--- sqlite-to-json/Arc-activities.sh
+++ sqlite-to-json/Arc-activities.sh
@@ -1,42 +1,25 @@
 #!/usr/bin/env bash
-# Imports visits of places from Arc app exports
+# Imports activities from Arc app exports
 
 ARC_PATH=${ARC_PATH:=/Users/$(whoami)/Library/Mobile\ Documents/iCloud~com~bigpaua~LearnerCoacher/Documents/Export/JSON/Monthly}
-
-# Load Foursquare categories when credentials are set
-# FOURSQUARE_VENUE_CATEGORIES_CACHE="./data/foursquare-venue-categories.json"
-# if [ -n "$FOURSQUARE_CLIENT_ID" ]; then
-#     if [ ! -f "$FOURSQUARE_VENUE_CATEGORIES_CACHE" ]; then
-#       curl -s -o "$FOURSQUARE_VENUE_CATEGORIES_CACHE" "https://api.foursquare.com/v2/venues/categories?client_id=$FOURSQUARE_CLIENT_ID&client_secret=$FOURSQUARE_CLIENT_SECRET&v=20210501"
-#     fi
-
-#     if [ -f "$FOURSQUARE_VENUE_CATEGORIES_CACHE" ]; then
-#       cat "$FOURSQUARE_VENUE_CATEGORIES_CACHE" | jq '.response.categories[] | {id, name}'
-#     fi
-# fi
 
 find "$ARC_PATH" -name '*.json.gz' -print0 \
 | xargs -0 gzip -dc \
 | TZ=Etc/UTC jq --raw-output -c '
   .timelineItems[]
-  | select(.isVisit == true)
+  | select(.isVisit == false and .uncertainActivityType == false)
   | {
     "id": ("arc-" + .itemId),
     "id_next": ("arc-" + .nextItemId),
     "id_previous": ("arc-" + .previousItemId),
-    "verb": "visited",
+    "verb": "moved",
     "provider": "Arc",
     "date_month": .endDate | fromdate | strftime("%Y-%m"),
     "timestamp_unix": .endDate | fromdate,
     "timestamp_utc": .endDate,
     "timestamp_start_unix": .startDate | fromdate,
     "timestamp_start_utc": .startDate,
-    "place_name": .place.name,
-    "place_address": .streetAddress,
-    "place_latitude": .center.latitude,
-    "place_longitude": .center.longitude,
-    "place_altitude": .altitude,
-    "place_foursquare_venue_id": .place.foursquareVenueId,
+    "activity_type": .activityType,
     "activity_step_count": .stepCount,
     "activity_floors_ascended": .floorsAscended,
     "activity_floors_descended": .floorsDescended,
@@ -44,5 +27,5 @@ find "$ARC_PATH" -name '*.json.gz' -print0 \
     "activity_heart_rate_max": .maxHeartRate,
     "activity_active_energy_burned": .activeEnergyBurned,
     }
-  '  \
+  ' \
   | jq -s -c '. | unique_by(.id) | .[]'
