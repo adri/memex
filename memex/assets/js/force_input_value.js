5aa90a624da6914bc747ca83b58dd35cab582e3a
Adrian Philipp <adrian@ticketswap.com>
2021-06-23 17:03:42 +0200

    Press CMD + K to focus the search input

--- memex/assets/js/force_input_value.js
+++ memex/assets/js/force_input_value.js
@@ -4,5 +4,13 @@ export const ForceInputValue = {
       "force-input-value",
       ({ value }) => (this.el.value = value)
     );
+
+    // CMD + K to focus on the search input
+    this.listener = document.addEventListener("keydown", (e) => {
+      if (e.code !== "KeyK" || e.metaKey === false) return;
+
+      this.el.focus();
+      this.el.select();
+    });
   },
 };
--- memex/lib/memex_web/live/components/search_bar.ex
+++ memex/lib/memex_web/live/components/search_bar.ex
@@ -7,7 +7,7 @@ defmodule MemexWeb.SearchBar do
     ~F"""
     <div class="p-5 font-san items-center justify-center">
         <header class="flex block fixed md:ml-4 text-black w-8/12 dark:text-white shadow-lg dark:bg-black bg-white rounded-xl">
-            <div class="flex justify-center items-center flex-grow block relative">
+            <div class="flex justify-center items-center flex-grow relative">
                 <button class="flex items-center justify-center absolute left-0 top-5 rounded-l-xl px-4 text-black dark:text-white">
                     <svg class="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
                          viewBox="0 0 24 24">
@@ -18,7 +18,8 @@ defmodule MemexWeb.SearchBar do
                     role="search" novalidate="">
                     <input
                         autocapitalize="off"
-                        autocomplete="off" autofocus="true"
+                        autocomplete="off"
+                        autofocus="true"
                         class="transition-colors focus:ring-blue-900 focus:ring-2 z-1 w-full text-black dark:text-white bg-transparent pl-10 pr-2 py-4 rounded-xl"
                         id="search-input"
                         maxlength="512"
@@ -30,6 +31,7 @@ defmodule MemexWeb.SearchBar do
                         type="search"
                         value={@query} />
                 </form>
+                <span class="absolute right-5 hidden sm:block text-gray-500 text-sm leading-5 py-0.5 px-1.5 border border-gray-700 rounded-md"><span class="sr-only">Press </span><kbd class="font-sans"><abbr title="Command" class="no-underline">⌘</abbr></kbd><span class="sr-only"> and </span><kbd class="font-sans">K</kbd><span class="sr-only"> to search</span></span>
             </div>
         </header>
     </div>
