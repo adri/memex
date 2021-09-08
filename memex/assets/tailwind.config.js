f89ed26c39ace15ea83b6a5a123685b1e80f3df4
Adrian Philipp <adrian@ticketswap.com>
2021-09-08 21:22:54 +0200

    Use gray color palette
    Fits better with the window chrome.

--- memex/assets/tailwind.config.js
+++ memex/assets/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -9,6 +11,16 @@ module.exports = {
   ],
   darkMode: "media", // false or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
     extend: {},
   },
   variants: {
