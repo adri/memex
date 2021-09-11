12c562988b6fbb9bd2c6a0f177746cc636fb3fea
Adrian Philipp <adrian@ticketswap.com>
2021-09-11 14:11:03 +0200

    Disable suggestions as it's removed for now

--- memex/lib/memex_web/live/components/search_bar.ex
+++ memex/lib/memex_web/live/components/search_bar.ex
@@ -14,7 +14,7 @@ defmodule MemexWeb.SearchBar do
                         <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>
                     </svg>
                 </button>
-                <form class="w-full relative" id="search-form" phx-change="search" phx-submit="search" phx-window-keyup="accept-suggestion"
+                <form class="w-full relative" id="search-form" phx-change="search" phx-submit="search"
                     role="search" novalidate="">
                     <input
                         autocapitalize="off"
