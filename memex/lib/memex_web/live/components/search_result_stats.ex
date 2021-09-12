c5e770a3595202afd32ee50756a168a5430f3589
Adrian Philipp <adrian@ticketswap.com>
2021-09-12 17:26:30 +0200

    Show search stats without performance
    Because now it's 3 different queries.

--- memex/lib/memex_web/live/components/search_result_stats.ex
+++ memex/lib/memex_web/live/components/search_result_stats.ex
@@ -1,18 +1,23 @@
 defmodule MemexWeb.SearchResultStats do
   use Surface.Component
 
-  prop(totalHits, :number)
-  prop(processingTimeMs, :number)
+  prop(total_hits, :number)
 
   def render(assigns) do
     ~F"""
-    <div :if={@totalHits != nil and @processingTimeMs != nil}
+    <div
       class="text-xs dark:text-gray-600 text-gray-400 ml-12 md:ml-20 mt-1">
       <div class="flex w-auto items-start">
         <div class="flex-none self-stretch border-l border-r dark:border-gray-800 border-gray-200 bg-gray-400 dark:bg-gray-700 w-2 -ml-1"></div>
 
         <div class="pl-3">
-          <span class="pt-4 inline-block">Found {@totalHits} results in {@processingTimeMs} ms</span>
+          <span class="pt-4 inline-block">
+          {#if @total_hits != nil }
+            Found {MemexWeb.TimelineView.number_short(@total_hits)} results
+          {#else}
+            &nbsp;
+          {/if}
+          </span>
         </div>
       </div>
     </div>
