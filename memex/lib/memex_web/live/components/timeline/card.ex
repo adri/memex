dcc2702a1d4c7207e731134dc52d00fd6478b64f
Adrian Philipp <adrian@ticketswap.com>
2021-09-29 17:34:43 +0200

    Card > Allow adding of main css-class

--- memex/lib/memex_web/live/components/timeline/card.ex
+++ memex/lib/memex_web/live/components/timeline/card.ex
@@ -1,18 +1,20 @@
 defmodule MemexWeb.Timeline.Card do
   use Surface.Component
 
+  prop class, :css_class
+
   @doc "Middle section content the card"
-  slot(content, required: true)
+  slot content, required: true
 
   @doc "Image to the left of the card"
-  slot(media)
+  slot media
 
   @doc "Section on the right of the card"
-  slot(right)
+  slot right
 
   def render(assigns) do
     ~F"""
-    <div class="flex-grow rounded-md bg-white dark:bg-gray-900 hover:border-blue-100 transition-colors p-4 my-2 ml-2 shadow-md overflow-hidden dark:text-white">
+    <div class={"#{@class} flex-grow rounded-md bg-white dark:bg-gray-900 hover:border-blue-100 transition-colors p-4 my-2 shadow-md overflow-hidden dark:text-white"}>
       <div class="float-right"><#slot name="right" /></div>
       <div class="float-left"><#slot name="media" /></div>
       <#slot name="content" />
