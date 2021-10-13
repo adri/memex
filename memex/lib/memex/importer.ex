8c2aae6ea9e3a24ba7d21bd16f7b562f12471de3
Adrian Philipp <adrian@ticketswap.com>
2021-10-13 12:20:38 +0200

    Extract importer logic to it's own module
    To reuse it for Elixir based importers.

---
+++ memex/lib/memex/importer.ex
@@ -0,0 +1,28 @@
+defmodule Memex.Importer do
+  @max_records_per_batch 20000
+
+  alias Memex.Repo
+  alias Memex.Schema.Document
+
+  def parse_body(""), do: {:error, :no_data}
+  def parse_body([]), do: {:error, :no_data}
+  def parse_body(body), do: {:ok, body |> Enum.map(&[body: &1])}
+
+  def bulk_upsert_documents(documents) do
+    {current_batch, next_batch} = Enum.split(documents, @max_records_per_batch)
+
+    Repo.insert_all(Document, current_batch,
+      on_conflict: {:replace, [:body]},
+      conflict_target: :id
+    )
+
+    case next_batch do
+      [] -> {:ok}
+      _ -> bulk_upsert_documents(next_batch)
+    end
+  end
+
+  def config(provider_id) do
+    Repo.get(Memex.Schema.ImporterConfig, provider_id).config
+  end
+end
--- memex/lib/memex_web/controllers/index_controller.ex
+++ memex/lib/memex_web/controllers/index_controller.ex
@@ -8,35 +8,16 @@ defmodule MemexWeb.IndexController do
 
   alias Memex.Schema.Document
   alias Memex.Repo
-
-  @max_records_per_batch 20000
+  alias Memex.Importer
 
   def upsert_documents(conn, %{"index" => _index} = params) do
-    with {:ok, documents} <- parse_body(params["_json"]),
-         {:ok} <- bulk_upsert_documents(documents) do
+    with {:ok, documents} <- Importer.parse_body(params["_json"]),
+         {:ok} <- Importer.bulk_upsert_documents(documents) do
       conn
       |> send_resp(200, "ok")
     else
       {:error, :no_data} -> send_resp(conn, 400, 'No content')
       {:error, error} -> send_resp(conn, 400, error)
-    end
-  end
-
-  defp parse_body(""), do: {:error, :no_data}
-  defp parse_body([]), do: {:error, :no_data}
-  defp parse_body(body), do: {:ok, body |> Enum.map(&[body: &1])}
-
-  defp bulk_upsert_documents(documents) do
-    {current_batch, next_batch} = Enum.split(documents, @max_records_per_batch)
-
-    Repo.insert_all(Document, current_batch,
-      on_conflict: {:replace, [:body]},
-      conflict_target: :id
-    )
-
-    case next_batch do
-      [] -> {:ok}
-      _ -> bulk_upsert_documents(next_batch)
     end
   end
 end
