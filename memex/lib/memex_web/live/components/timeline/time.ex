90df1f8e662f32f2d3d54555bd274cd9ac4f6b32
Adrian Philipp <adrian@ticketswap.com>
2022-08-09 15:45:10 +0200

    Filter on time in the UI

--- memex/lib/memex_web/live/components/timeline/time.ex
+++ memex/lib/memex_web/live/components/timeline/time.ex
@@ -5,11 +5,16 @@ defmodule MemexWeb.Timeline.Time do
 
   def render(assigns) do
     ~F"""
-    <div class="flex-none self-center text-right text-gray-400 dark:text-gray-500 text-xs md:text-md -ml-12 md:-ml-32 w-12 md:w-32 pr-5">
+    <div
+      phx-click="filter-reset"
+      phx-value-key="time"
+      phx-value-value={"\"#{to_iso(@date)}\""}
+      class="flex-none cursor-pointer self-center text-right text-gray-400 dark:text-gray-500 text-xs md:text-md -ml-12 md:-ml-32 w-12 md:w-32 pr-5">
       {to_time(@date)}
     </div>
     """
   end
 
   defp to_time(date), do: Calendar.strftime(date, "%H:%M")
+  defp to_iso(date), do: DateTime.to_iso8601(date)
 end
