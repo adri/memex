9704ec09a63b42a49577917a786e7c98975d0a24
Adrian Philipp <adrian@ticketswap.com>
2023-05-17 20:03:35 +0200

    Cleanup

--- memex/lib/memex_web.ex
+++ memex/lib/memex_web.ex
@@ -35,21 +35,6 @@ defmodule MemexWeb do
     end
   end
 
-  # def view do
-  #   quote do
-  #     use Phoenix.View,
-  #       root: "lib/memex_web/templates",
-  #       namespace: MemexWeb
-
-  #     # Import convenience functions from controllers
-  #     import Phoenix.Controller,
-  #       only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
-
-  #     # Include shared imports and aliases for views
-  #     unquote(view_helpers())
-  #   end
-  # end
-
   def live_view do
     quote do
       use Phoenix.LiveView,
