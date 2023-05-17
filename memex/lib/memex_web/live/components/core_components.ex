3a4ce4137b30ac321d8d04b0a2f60fbd09b0bdd7
Adrian Philipp <adrian@ticketswap.com>
2023-05-17 16:50:04 +0200

    Fix styles

--- memex/lib/memex_web/live/components/core_components.ex
+++ memex/lib/memex_web/live/components/core_components.ex
@@ -139,7 +139,7 @@ defmodule MemexWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed hidden top-2 right-2 w-80 sm:w-96 z-50 rounded-lg p-3 shadow-md shadow-zinc-900/5 ring-1 dark:bg-gray-500",
+        "fixed hidden top-2 right-2 w-80 sm:w-96 z-50 rounded-lg p-3 shadow-md shadow-zinc-900/5 dark:bg-gray-200",
         @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
         @kind == :error && "bg-rose-50 p-3 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
       ]}
@@ -177,7 +177,7 @@ defmodule MemexWeb.CoreComponents do
     <.flash
       id="disconnected"
       kind={:error}
-      title="We can't find the internet"
+      title="No connection"
       close={false}
       autoshow={false}
       phx-disconnected={show("#disconnected")}
