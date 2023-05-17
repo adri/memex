018ff513bae71972a25fa1fd32363ff506049576
Adrian Philipp <adrian@ticketswap.com>
2023-05-17 17:08:30 +0200

    Add missing mix commands

--- memex/mix.exs
+++ memex/mix.exs
@@ -76,6 +76,9 @@ defmodule Memex.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": [
