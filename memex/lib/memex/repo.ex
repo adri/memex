4fa09707080f08d702fa1f2f70072c1d6c56b306
Adrian Philipp <adrian@ticketswap.com>
2023-07-29 13:59:50 +0200

    Nice helper to execute multiple statements in a transaction

--- memex/lib/memex/repo.ex
+++ memex/lib/memex/repo.ex
@@ -2,4 +2,26 @@ defmodule Memex.Repo do
   use Ecto.Repo,
     otp_app: :memex,
     adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  A small wrapper around `Repo.transaction/2'.
+
+  Commits the transaction if the lambda returns `{:ok, result}`, rolling it
+  back if the lambda returns `{:error, reason}`. In both cases, the function
+  returns the result of the lambda.
+  """
+  @spec transact((() -> any()), keyword()) :: {:ok, any()} | {:error, any()}
+  def transact(fun, opts \\ []) do
+    transaction(
+      fn ->
+        case fun.() do
+          {:ok, value} -> value
+          :ok -> :transaction_commited
+          {:error, reason} -> rollback(reason)
+          :error -> rollback(:transaction_rollback_error)
+        end
+      end,
+      opts
+    )
+  end
 end
