c9a2cebfef0b5029d0cbcdff93a8f8c84ab3d48b
Adrian Philipp <adrian@ticketswap.com>
2023-11-30 16:17:10 +0100

    Avoid duplicate search call

--- memex/lib/memex_web/components/search_bar.ex
+++ memex/lib/memex_web/components/search_bar.ex
@@ -19,14 +19,7 @@ defmodule MemexWeb.SearchBar do
               <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
             </svg>
           </button>
-          <form
-            class="w-full relative"
-            id="search-form"
-            phx-change="search"
-            phx-submit="search"
-            role="search"
-            novalidate=""
-          >
+          <form class="w-full relative" id="search-form" role="search" novalidate="">
             <div
               id="editor"
               phx-update="ignore"
